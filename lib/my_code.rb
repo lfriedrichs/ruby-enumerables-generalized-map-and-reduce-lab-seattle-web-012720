950212e8b9d7acb4d01c1503cd2aa5ff91038ae9
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-17 01:08:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(source_array)
+  index = 0 
+  new_array = Array.new
+  while index < source_array.size 
+    new_array[index] = yield(source_array[index])
+    index += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    new_value = 0 
+  else index = 0 
+  new_value = starting_point
+  while index < source_array.size 
+    new_value = yield(new_value, source_array[index])
+    index += 1
+  end
+  new_value
+end
